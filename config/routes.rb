e8734560e882e33096020a872508cff9851953f5
mlubas <michael.lubas@betterment.com>
2018-10-22 19:07:49 -0400

    add users controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
     get 'sign_in', to: 'devise/sessions#new', as: :new_user_session
     get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
-
   root to: "hello#index"
   get 'about', action: :show, controller: 'abouts'
+  get 'profile', action: :show, controller: 'profiles'
+  resources :users 
 end
