12f6e52604849412423605811cdb78f3f595ff55
Dan Bound-Black <dan.boundblack@betterment.com>
2018-10-29 19:20:31 -0400

    fixes about form submission

--- app/controllers/abouts_controller.rb
+++ app/controllers/abouts_controller.rb
@@ -8,12 +8,17 @@ class AboutsController < ApplicationController
   end
   def update
   	# puts params[:user]
-  	current_user.update! params[:user]
+  	if current_user.update! user_params
+      flash[:success] = "Yay! You did it!"
+      redirect_to about_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def user_params
-  	params.permit(:user)
+  	params.require(:user).permit([:favorite_wine, :favorite_movie, :about_me])
   end
 end
