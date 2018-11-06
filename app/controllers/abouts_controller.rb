e7045be65785d752a6f287d69cad0880cd77c24b
mlubas <michael.lubas@betterment.com>
2018-11-05 19:34:10 -0500

    show posts on aboutn page

--- app/controllers/abouts_controller.rb
+++ app/controllers/abouts_controller.rb
@@ -2,6 +2,7 @@ class AboutsController < ApplicationController
   # THIS MAPS to show.html.erb
   def show # maps to views > about > show.html.erb
     @user = current_user
+    @posts = @user.posts
   end
   def edit 
   	@user = current_user
